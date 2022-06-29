eb6c4f03fec9b55941d143a711fb3ec3d96426cc
gabyse1 <gaby.es.iaa@gmail.com>
2022-06-29 18:31:25 -0500

    Add TourItemDetails routes to the App component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import '../scss/App.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
+import TourItemDetails from './TourItemDetails';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/tours/:name" element={<TourItemDetails />} />
+      <Route path="*" element={<Home />} />
     </Routes>
   </BrowserRouter>
 );
