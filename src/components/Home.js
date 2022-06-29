31652320f959f495ed5c5fb1dc9ebf92b4ea11b3
gabyse1 <gaby.es.iaa@gmail.com>
2022-06-29 16:29:13 -0500

    Create the Home or Main page component.

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import ToursList from './ToursList';
+
+const Home = () => (
+  <div className="section__main">
+    <h1>MOST POPULAR TOURS</h1>
+    <p>Please select a Tour</p>
+    <ToursList />
+  </div>
+);
+
+export default Home;
---
+++ src/components/TourItem.js
@@ -0,0 +1,38 @@
+import { PropTypes } from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const TourItem = ({ tourItem }) => (
+  <li className="list__item column">
+    <div className="card">
+      <div className="card__picture">
+        <img src={tourItem.image_url} alt={'tour image '.concat(tourItem.id)} />
+      </div>
+      <h2 className="card__heading">
+        <span className="card__heading-span">{tourItem.name}</span>
+      </h2>
+      <div className="card__details">
+        <ul>
+          <li>
+            {`${tourItem.duration} days tour`}
+          </li>
+          <li>
+            {'Difficulty: '.concat(tourItem.difficulty)}
+          </li>
+        </ul>
+      </div>
+      <Link to={`/details/${tourItem.name.replace(/ /, '-')}`} state={tourItem} className="card__link" />
+    </div>
+  </li>
+);
+
+TourItem.propTypes = {
+  tourItem: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    duration: PropTypes.number.isRequired,
+    difficulty: PropTypes.string.isRequired,
+    image_url: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default TourItem;
