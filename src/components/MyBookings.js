13ba36314037cfb74f4807fcbb64a920be2cf68f
gabyse1 <gaby.es.iaa@gmail.com>
2022-06-30 16:02:55 -0500

    Creathe components to render My Reservations page.

---
+++ src/components/MyBookings.js
@@ -0,0 +1,28 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import MyBookingItem from './MyBookingItem';
+import { getBookingsAPI } from '../redux/bookings/bookingsReducer';
+
+const MyBookings = () => {
+  const dispatch = useDispatch();
+  const myBookingsList = useSelector((store) => store.bookingsReducer);
+
+  useEffect(() => {
+    dispatch(getBookingsAPI(1));
+  }, []);
+
+  return (
+    <div className="section__main">
+      <ul className="tours__list row">
+        {
+          myBookingsList.length > 0 && myBookingsList.map(
+            (booking) => <MyBookingItem key={uuidv4()} bookingItem={booking} />,
+          )
+        }
+      </ul>
+    </div>
+  );
+};
+
+export default MyBookings;
--- src/components/MyReservations.js
+++
@@ -1,7 +0,0 @@
-const MyReservations = () => (
-  <div className="section__main">
-    <h1>MY RESERVATIONS COMPONENT.</h1>
-  </div>
-);
-
-export default MyReservations;
