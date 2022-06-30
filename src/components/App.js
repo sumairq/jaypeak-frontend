bb18e7d880e6d20d4e444a35ecc97f9ebbbcfe60
mukhammadrizooff <legend.it@bk.ru>
2022-06-30 21:31:10 +0500

    Add route to App.js file

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './Home';
 import TourItemDetails from './TourItemDetails';
+import ToursDeleteList from './ToursDeleteList';
 
 const App = () => (
   <BrowserRouter>
@@ -10,6 +11,7 @@ const App = () => (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/tours/:name" element={<TourItemDetails />} />
+      <Route path="/delete" element={<ToursDeleteList />} />
       <Route path="*" element={<Home />} />
     </Routes>
   </BrowserRouter>
